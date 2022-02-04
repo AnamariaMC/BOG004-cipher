a993e35a6c04106a8aca9968a90433982dfc499e
AnamariaMC <anymedinac14@gmail.com>
2022-02-03 22:12:04 -0500

    modificación final

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,25 @@
 const cipher = {
   //   encode, decode
   encode (casillas, texto){
-    if (!texto)
-      return " ";
+    if (typeof texto !== 'string' || texto.length === 0
+    ){
+        throw new TypeError("Por favor ingresa un texto o palabra");
+    }
+    /*else if (typeof casillas !== 'number' || casillas.length === 0
+    ){
+       throw new TypeError("Por favor ingresa un numero de casillas");
+    }*/
       let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       casillas = (casillas % 26+26) % 26;
       return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c)+casillas)%26]);
   },
+
   decode (casillas, texto){
-    if (!texto)
-      return "";
+    if (typeof texto !== 'string' || texto.length === 0
+    ){
+        throw new TypeError("Por favor ingresa un texto o palabra");
+    }
+    
       let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       casillas = (casillas % 26-26) % 26;
       return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c)-casillas)%26]);
